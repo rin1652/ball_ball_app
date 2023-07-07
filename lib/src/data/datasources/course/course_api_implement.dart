a76bc887c2b98d3c36544ba29d2f67ce2aa7112f
Phuc Nguyen <ndphuc1903@gmail.com>
2023-07-07 09:11:19 +0700

    add http_service

--- lib/src/data/datasources/course/course_api_implement.dart
+++ lib/src/data/datasources/course/course_api_implement.dart
@@ -1,78 +1,33 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:ball_ball/src/utils/constants/app_api_url.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../domain/models/course.dart';
 import 'course_api.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 final courseApiProvider = Provider<CourseApi>((ref) => CourseApiImplement());
 
-class CourseApiImplement implements CourseApi {
+class CourseApiImplement extends CourseApi {
+  final String urlCourse = AppApiUrl.course;
   @override
   Future<List<Course>> getListCourse() async {
-    final url = Uri.parse(AppApiUrl.course);
-    final response = await http.get(url, headers: {
-      HttpHeaders.authorizationHeader: 'key abc',
-    });
-
-    if (response.statusCode == 200) {
-      final List<dynamic> list = json.decode(response.body);
-      return Course.fromList(list);
-    } else {
-      throw Exception('Failed to load course');
-    }
+    final Response bodyResponse = await getCourseService(urlCourse);
+    final List<dynamic> list = json.decode(bodyResponse.body);
+    return Course.fromList(list);
   }
 
   @override
   Future<void> createCourse(Course course) async {
-    final url = Uri.parse(AppApiUrl.course);
-    final response = await http.post(url,
-        headers: {
-          HttpHeaders.authorizationHeader: 'key abc',
-        },
-        body: Course.toMap(course));
-
-    if (response.statusCode == 201) {
-      // print(response.body);
-      // final List<dynamic> list = json.decode(response.body);
-
-      // return Course.fromList(list);
-    } else {
-      throw Exception('Failed to load course');
-    }
+    await postCourseService(urlCourse, course);
   }
 
   @override
   Future<void> deleteCourse(int id) async {
-    final url = Uri.parse('${AppApiUrl.course}/$id');
-    final response = await http.delete(
-      url,
-      headers: {
-        HttpHeaders.authorizationHeader: 'key abc',
-      },
-    );
-
-    if (response.statusCode == 200) {
-    } else {
-      throw Exception('Failed to load course');
-    }
+    await deleteCourseService(urlCourse, id);
   }
 
   @override
   Future<void> updateCourse(Course course) async {
-    final url = Uri.parse('${AppApiUrl.course}/${course.id}');
-    final response = await http.delete(
-      url,
-      headers: {
-        HttpHeaders.authorizationHeader: 'key abc',
-      },
-      body: Course.toMap(course),
-    );
-
-    if (response.statusCode == 200) {
-    } else {
-      throw Exception('Failed to load course');
-    }
+    await updateCourseService(urlCourse, course, course.id);
   }
 }
